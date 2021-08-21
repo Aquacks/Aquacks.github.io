f0426e1bece9d3a9f090828047f2b90dbca947ac
Aquacks <ejrowett@gmail.com>
2021-08-21 20:46:05 +0100

    amoguslol

--- JS/itemsScript.js
+++ JS/itemsScript.js
@@ -1,5 +1,8 @@
 var addItemButton = document.getElementsByClassName("addToCartButton")
 console.log(addItemButton)
 for (var i = 0; i < addItemButton.length; i++){
-
+    var button = addItemButton[i]
+    button.addEventListener("click", function(){
+        console.log("clicked")
+    })
 }

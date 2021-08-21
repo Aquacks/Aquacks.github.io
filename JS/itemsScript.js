c146e20363f20848cdc258ebcb981ec0125c5e37
Aquacks <ejrowett@gmail.com>
2021-08-21 21:15:02 +0100

    WOO IM RESTARTING THE ENTIRE ITEMS SECTION YAYYY

--- JS/itemsScript.js
+++ JS/itemsScript.js
@@ -4,5 +4,8 @@ for (var i = 0; i < addItemButton.length; i++){
     var button = addItemButton[i]
     button.addEventListener("click", function(){
         console.log("clicked")
+        var cart1 = document.getElementsByTagName()
+        localStorage.setItem("cart1", )
     })
 }
+

9bcf0379e04db8c04695b63088984a3e56f04f21
Aquacks <ejrowett@gmail.com>
2021-08-21 20:40:33 +0100

    com

--- JS/itemsScript.js
+++ JS/itemsScript.js
@@ -1,3 +1,5 @@
 var addItemButton = document.getElementsByClassName("addToCartButton")
 console.log(addItemButton)
-for (var i = 0; i < addItemButton.length)
+for (var i = 0; i < addItemButton.length; i++){
+
+}

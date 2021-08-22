0910fe72d9ece54a5ef6886999a424f56aa58a4a
Aquacks <ejrowett@gmail.com>
2021-08-22 17:08:50 +0100

    js

--- JS/cartScript.js
+++ JS/cartScript.js
@@ -2,9 +2,5 @@ var cartList = document.getElementById("content1")
 console.log(cartList)
 
 for(var i = 0; localStorage.length; ++i){
-    const div = document.createElement("div")
-    const divText = document.createTextNode(localStorage[i])
-    div.appendChild(divText)
-    const currentDiv = document.getElementById("content1")
-    document.body.insertBefore(div, currentDiv)
+    
 }

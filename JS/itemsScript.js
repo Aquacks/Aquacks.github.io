e04ce0cf6e8b9cf67d44691271b8ba6b7f0aef33
Aquacks <ejrowett@gmail.com>
2021-08-22 12:59:38 +0100

    js

--- JS/itemsScript.js
+++ JS/itemsScript.js
@@ -1,12 +1,18 @@
 var addItemButton = document.getElementsByClassName("addToCartBtn")
 console.log(addItemButton)
+
+var cartN = 0
+
 for (var i = 0; i < addItemButton.length; i++){
     var button = addItemButton[i]
     button.addEventListener("click", function(){
         console.log("clicked")
-        var itemName = button.parentElement.getElementsByTagName("h1") 
-        var itemPrice = button.parentElement.getElementsByTagName("h2")
-        localStorage.setItem("cart1", itemName[0].innerText.concat(",").concat(itemPrice[0].innerText))  
+        if(cartN < 13){
+            cartN = cartN + 1
+            var itemName = button.parentElement.getElementsByTagName("h1") 
+            var itemPrice = button.parentElement.getElementsByTagName("h2")
+            localStorage.setItem("cart"+cartN, itemName[0].innerText.concat(",").concat(itemPrice[0].innerText)) 
+        } 
     })
 }
 

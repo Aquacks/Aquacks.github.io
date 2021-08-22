efdc9582bc4626334d6f073edf011c6191f0fb51
Aquacks <ejrowett@gmail.com>
2021-08-22 19:45:02 +0100

    js

--- JS/cartScript.js
+++ JS/cartScript.js
@@ -8,16 +8,16 @@ function updateCart(){
 
         var cartN = i + 1
 
-        var h2 = document.createElement("h2")
-        li.appendChild(h2)
-        h2.setAttribute("id", "price")
-        var text2 = localStorage.getItem("cart"+cartN)
-        text2 = text2.split(",").pop()
-        h2.innerText = text2
-
         ul.appendChild(li)
         var text = localStorage.getItem("cart"+cartN)
         text = text.split(",").shift()
         li.innerText = text
+        
+        var h2 = document.createElement("h2")
+        h2.setAttribute("id", "price")
+        li.appendChild(h2)
+        var text2 = localStorage.getItem("cart"+cartN)
+        text2 = text2.split(",").pop()
+        h2.innerText = text2
     }
 }

296d126d6a99782f3e466ef6a7d822c29e3ed636
Aquacks <ejrowett@gmail.com>
2021-08-22 12:10:38 +0100

    js

--- JS/itemsScript.js
+++ JS/itemsScript.js
@@ -1,11 +1,11 @@
-var addItemButton = document.getElementsByClassName("addToCartButton")
+var addItemButton = document.getElementsByClassName("addToCartBtn")
 console.log(addItemButton)
 for (var i = 0; i < addItemButton.length; i++){
     var button = addItemButton[i]
     button.addEventListener("click", function(){
         console.log("clicked")
         var cart1 = button.parentElement.innerText
-        localStorage.setItem("cart1", cart1)
+        localStorage.setItem("cart1", cart1.toString)
     })
 }
 

5e9581773be1b6d651610a9537e1d111c8e3d737
Aquacks <ejrowett@gmail.com>
2021-08-22 17:25:51 +0100

    js

--- JS/cartScript.js
+++ JS/cartScript.js
@@ -1,4 +1,8 @@
-
-for(var i = 0; localStorage.length; i++){
-    console.log(i)
+window.addEventListener('load', function () {
+    printLength()
+})
+function printLength(){
+    for(var i = 0; localStorage.length; i++){
+        console.log(i)
+    }
 }

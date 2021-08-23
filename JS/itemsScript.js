2996a82e95027015700b37dfba8e2b268ddb9674
Aquacks <ejrowett@gmail.com>
2021-08-23 20:28:22 +0100

    js

--- JS/itemsScript.js
+++ JS/itemsScript.js
@@ -4,15 +4,13 @@ console.log(addItemButton)
 for (var i = 0; i < addItemButton.length; i++){
     var button = addItemButton[i]
     console.log(button)
-    button.addEventListener("click", function(){
+    button.addEventListener("click", function(event){
+        var buttonClicked = event.target
+        var itemName = buttonClicked.parentElement.querySelector("h1").innerText
         console.log("clicked")
         if(localStorage.length < 12){
-            var itemsName = button.parentElement.getElementsByTagName("h1")
-            var itemsPrice = button.parentElement.getElementsByTagName("h2")
-            console.log(itemsPrice)
-            console.log(itemsName)
-            var itemName = itemsName[i].innerText
-            var itemPrice = itemsPrice[i].innerText
+            
+
             localStorage.setItem("item"+(localStorage.length + 1), itemName)
         } 
         else alert("Maximum of 12 shulkers per order")

5ac86217ea8198146ae1e5b72d24e3f8826e3604
Aquacks <ejrowett@gmail.com>
2021-08-23 18:46:08 +0100

    oh god

--- JS/itemsScript.js
+++ JS/itemsScript.js
@@ -1,17 +1,19 @@
 var addItemButton = document.getElementsByClassName("addToCartBtn")
 console.log(addItemButton)
 
-for (var i = 0; i < addItemButton.length; i++){
-    var button = addItemButton[i]
-    console.log(i)
-    button.addEventListener("click", function(){
-        console.log("clicked")
-        if(localStorage.length < 12){
-            var itemName = button.parentElement.getElementsByTagName("h1") 
-            var itemPrice = button.parentElement.getElementsByTagName("h2")
-            console.log(itemName)
-            localStorage.setItem("item"+(localStorage.length + 1), itemName[0].innerText.concat(",").concat(itemPrice[0].innerText)) 
-        } 
-        else alert("Maximum of 12 shulkers per order")
-    })
+while(true){
+    for (var i = 0; i < addItemButton.length; i++){
+        var button = addItemButton[i]
+        console.log(i)
+        button.addEventListener("click", function(){
+            console.log("clicked")
+            if(localStorage.length < 12){
+                var itemName = button.parentElement.getElementsByTagName("h1") 
+                var itemPrice = button.parentElement.getElementsByTagName("h2")
+                console.log(itemName)
+                localStorage.setItem("item"+(localStorage.length + 1), itemName[0].innerText.concat(",").concat(itemPrice[0].innerText)) 
+            } 
+            else alert("Maximum of 12 shulkers per order")
+        })
+    }
 }

edaf06050eaae23a4024efc32bf98e74c2f85af5
Aquacks <ejrowett@gmail.com>
2021-08-23 18:01:06 +0100

    js

--- JS/cartScript.js
+++ JS/cartScript.js
@@ -6,8 +6,7 @@ function updateCart(){
         addCartItem()
     }
 }
-function addCartItem{
-    
+function addCartItem(){
         var ul = document.getElementById("itemsList")
         var li = document.createElement("li")
 

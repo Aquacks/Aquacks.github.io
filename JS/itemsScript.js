d4d924605092b2746bbf3ee735d5143c55e2e545
Aquacks <ejrowett@gmail.com>
2021-08-23 19:56:06 +0100

    js

--- JS/itemsScript.js
+++ JS/itemsScript.js
@@ -9,6 +9,8 @@ for (var i = 0; i < addItemButton.length; i++){
         if(localStorage.length < 12){
             var itemsName = button.parentElement.getElementsByTagName("h1")
             var itemsPrice = button.parentElement.getElementsByTagName("h2")
+            console.log(itemsPrice)
+            console.log(itemsName)
             var itemName = itemsName[i].innerText
             var itemPrice = itemsPrice[i].innerText
             localStorage.setItem("item"+(localStorage.length + 1), itemName)

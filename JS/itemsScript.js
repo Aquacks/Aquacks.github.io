c42a59b1c9e11b9287f012256b9c544adb0da52e
Aquacks <ejrowett@gmail.com>
2021-08-23 18:23:22 +0100

    js

--- JS/itemsScript.js
+++ JS/itemsScript.js
@@ -1,22 +1,15 @@
-window.addEventListener('load', function () {
-    addToLocal()
-})
+var addItemButton = document.getElementsByClassName("addToCartBtn")
+console.log(addItemButton)
 
-function addToLocal(){
-    var addItemButton = document.getElementsByClassName("addToCartBtn")
-    console.log(addItemButton)
-    var cartN = 0
-    for (var i = 0; i < addItemButton.length; i++){
-        var button = addItemButton[i]
-        button.addEventListener("click", function(){
-            console.log("clicked")
-            if(cartN < 12){
-                cartN = cartN + 1
-                var itemName = button.parentElement.getElementsByTagName("h1") 
-                var itemPrice = button.parentElement.getElementsByTagName("h2")
-                localStorage.setItem("cart"+cartN, itemName[0].innerText.concat(",").concat(itemPrice[0].innerText)) 
-            } 
-            else alert("Maximum of 12 shulkers per order")
-        })
-    }
+for (var i = 0; i < addItemButton.length; i++){
+    var button = addItemButton[i]
+    button.addEventListener("click", function(){
+        console.log("clicked")
+        if(localStorage.length < 12){
+            var itemName = button.parentElement.getElementsByTagName("h1") 
+            var itemPrice = button.parentElement.getElementsByTagName("h2")
+            localStorage.setItem("item"+i, itemName[0].innerText.concat(",").concat(itemPrice[0].innerText)) 
+        } 
+        else alert("Maximum of 12 shulkers per order")
+    })
 }

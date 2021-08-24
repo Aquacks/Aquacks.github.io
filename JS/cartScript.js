87f4b0608083ae1826ce54f4b1a172bda42ef354
Aquacks <ejrowett@gmail.com>
2021-08-24 17:24:35 +0100

    js

--- JS/cartScript.js
+++ JS/cartScript.js
@@ -44,6 +44,8 @@ function runDeleteButtons(){
         removeButton.addEventListener("click", function(event) {
             var removeBtnClicked = event.target
             console.log(removeBtnClicked)
+            console.log(removeBtnClicked.id)
+            console.log(localStorage.getItem("item")+removeBtnClicked.id)
             removeBtnClicked.parentElement.parentElement.remove()
             if((localStorage.getItem("item")+removeBtnClicked.id).includes(removeBtnClicked.parentElement.parentElement.innerText)){
                 localStorage.remove("item", removeBtnClicked.id)

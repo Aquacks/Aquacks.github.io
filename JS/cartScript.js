5cd04b1346f3a675d78fc9f17fa091b6e1031d80
Aquacks <ejrowett@gmail.com>
2021-08-24 15:47:21 +0100

    js

--- JS/cartScript.js
+++ JS/cartScript.js
@@ -4,14 +4,14 @@ window.addEventListener('load', function () {
 })
 function createItemEntry(){
     for(var i = 0; i < localStorage.length; i++){
+        var itemN = i + 1
+        if(localStorage.getItem("item"+itemN) != null){
         var ul = document.getElementById("itemsList")
         var li = document.createElement("li")
 
         var h2 = document.createElement("h2")
         var deleteBtn = document.createElement("button")
         var deleteBtnImg = document.createElement("img")
-
-        var itemN = i + 1
 
         ul.appendChild(li)
         var text = localStorage.getItem("item"+itemN)
@@ -32,6 +32,7 @@ function createItemEntry(){
         var text2 = localStorage.getItem("item"+itemN)
         text2 = text2.split(",").pop()
         h2.innerText = text2
+        }
     }
 }
 function runDeleteButtons(){

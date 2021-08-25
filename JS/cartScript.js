9ca355af718339c4d849bec41b4e92b2bec3ccd9
Aquacks <ejrowett@gmail.com>
2021-08-25 19:12:25 +0100

    js

--- JS/cartScript.js
+++ JS/cartScript.js
@@ -3,9 +3,9 @@ window.addEventListener('load', function () {
     runDeleteButtons()
 })
 function createItemEntries(){
-    for(var i = 0; i < localStorage.length; i++){//todo: length of storage does not equal item entry number
+    for(var i = 0; i < 12; i++){//todo: length of storage does not equal item entry number
         var itemN = i + 1
-        if(localStorage.getItem("item"+itemN) != null){
+        if(localStorage.getItem("item"+itemN) != "0" || localStorage.getItem("item"+itemN) != null){
             console.log("creating")
             var ul = document.getElementById("itemsList")
             var li = document.createElement("li")
@@ -38,21 +38,17 @@ function createItemEntries(){
     }
 }
 function runDeleteButtons(){
-    var removeCartBtns = document.getElementsByClassName("deleteBtn")
+    var removeCartBtns = document.getElementsByClassName("deleteBtn") //assigns all buttons to "removeCartBtns"
     console.log(removeCartBtns)
-    for(var i = 0; i < removeCartBtns.length; i++){
-        var removeButton = removeCartBtns[i]
-        removeButton.addEventListener("click", function(event) {
-            var removeBtnClicked = event.target
+    for(var i = 0; i < removeCartBtns.length; i++){ //for the number of buttons there are
+        var removeButton = removeCartBtns[i] //assign each button to an individual variable
+        removeButton.addEventListener("click", function(event) { //when any button is clicked
+            var removeBtnClicked = event.target //the one that has been clicked is asigned to variable
             console.log(removeBtnClicked)
             console.log(removeBtnClicked.parentElement)
-            removeBtnClicked.parentElement.parentElement.remove()
-            localStorage.removeItem("item"+removeBtnClicked.parentElement.id)
+            removeBtnClicked.parentElement.parentElement.remove() //remove html entry for item
+            localStorage.setItem(("item"+removeBtnClicked.parentElement.id),"0") //set localstorage entry for item to "0"
             console.log("removed")
-
-            // if((localStorage.getItem("item")+removeBtnClicked.parentElement.id).includes(removeBtnClicked.parentElement.parentElement.innerText)){
-            //     localStorage.remove("item"+removeBtnClicked.parentElement.id)
-            // }
         })
     }
 }
---
+++ JS/main.js
@@ -0,0 +1,3 @@
+for(var i = 1; i < 12; i++){
+    localStorage.setItem(("item"+i), "0")
+}

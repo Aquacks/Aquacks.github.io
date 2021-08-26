f5eb8fa725fdbc6819ae2e8ce26922e6c1ff9ff6
Aquacks <ejrowett@gmail.com>
2021-08-26 22:52:19 +0100

    jshtml

--- JS/cartScript.js
+++ JS/cartScript.js
@@ -73,14 +73,7 @@ function runDeliveryModal(){
 function getOptionsData(){
     console.log(document.getElementById('xcoord').value)
     console.log(document.getElementById('zcoord').value)
-    const rbs = document.querySelectorAll('input[name="deliveryOption"]');
-    let selectedValue;
-    for (const rb of rbs) {
-        if (rb.checked) {
-            selectedValue = rb.value;
-            break;
-        }
-    }
+    if(document.getElementById("dungeonBool").)
     console.log(selectedValue)
     return false;
 }

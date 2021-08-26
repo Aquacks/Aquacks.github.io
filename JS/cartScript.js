f39371fdc037fb821302cf9611f56be3b1caf096
Aquacks <ejrowett@gmail.com>
2021-08-26 17:43:32 +0100

    js

--- JS/cartScript.js
+++ JS/cartScript.js
@@ -59,7 +59,7 @@ function runDeliveryModal(){
     var span = document.getElementsByClassName("close")[0];
 
     btn.onclick = function() {
-        modal.style.display = "block";
+        modal.style.display = "flex";
     }
     span.onclick = function() {
         modal.style.display = "none";

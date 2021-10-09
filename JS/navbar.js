ae94e5117ccfde7829bf9ed607123cb722b04455
Aquacks <ejrowett@gmail.com>
2021-10-10 00:30:55 +0100

    js

--- JS/navbar.js
+++ JS/navbar.js
@@ -2,15 +2,23 @@
 
 var queueElement = document.getElementById("queueCount");
 
-fetch("https://2bqueue.info/queue", {mode: "no-cors"})
-    .then((res) => res.json())
-    .then((json) => {
-        var textDiv = document.createElement("div");
-        textDiv.innerHTML = `Queue: ${json.regular}`;
-        queueElement.appendChild(textDiv)
-    })
-    .catch(err => {
-        console.error('Error: ', err);
-    });
+const interval = setInterval(function() {
+    update();
+}, 10000);
 
+function update() {
+    fetch("https://2bqueue.info/queue", {mode: "no-cors"})
+        .then((res) => res.json())
+        .then((json) => {
+            var textDiv = document.createElement("div");
+            textDiv.innerHTML = `Queue: ${json.regular}`;
+            if(textDiv != null){
+                queueElement.appendChild(textDiv)
+            }
+            else return
+        })
+        .catch(err => {
+            console.error('Error: ', err);
+        });
+}
 // End Queue Display

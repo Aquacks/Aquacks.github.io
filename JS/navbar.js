57a5ecc003ffda9ba609cfe5a0afe45ecc55cc28
Aquacks <ejrowett@gmail.com>
2021-10-10 00:20:13 +0100

    js

--- JS/navbar.js
+++ JS/navbar.js
@@ -3,8 +3,8 @@
 var queueElement = document.getElementById("queueCount");
 
 fetch("https://2bqueue.info/queue", {mode: "no-cors"})
-    .then(res => res.json)
-    .then(json => {
+    .then((res) => res.json())
+    .then((json) => {
         var textDiv = document.createElement("div");
         textDiv.innerHTML = `Queue: ${json.regular}`;
         queueElement.appendChild(textDiv)

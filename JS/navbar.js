1be2a5283a49e760f7f0c4d07ba6858446af69ee
Aquacks <ejrowett@gmail.com>
2021-10-09 23:20:47 +0100

    js

--- JS/navbar.js
+++ JS/navbar.js
@@ -3,7 +3,9 @@ var queueElement = document.getElementById("queueCount");
 fetch("https://2bqueue.info/queue", {mode: "no-cors"})
     .then(res => res.json())
     .then(data => {
-        queueElement.innerHTML = data
+        var textDiv = document.createElement("div");
+        textDiv.innerHTML = dat[0];
+        queueElement.appendChild(textDiv)
     })
     .catch(err => {
         console.error('Error: ', err);

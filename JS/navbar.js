2ac756801ec82853a686c1891816cbdf3b2c1a6f
Aquacks <ejrowett@gmail.com>
2021-10-10 16:56:28 +0100

    js

--- JS/navbar.js
+++ JS/navbar.js
@@ -2,7 +2,11 @@
 var queueElement = document.getElementById("queueCount");
 
 function getQueue() {
-    fetch("https://2bqueue.info/*", {mode: "cors"})
+    fetch("https://2bqueue.info/*", {mode: "no-cors"}, {method: "GET"}, {headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials':true,
+        'Access-Control-Allow-Methods':'POST, GET'
+    }})
     .then(res => console.log(res))
 }
 getQueue();

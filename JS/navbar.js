9009ac691331e8e6d83865f4c7907f9e2a4f1821
Aquacks <ejrowett@gmail.com>
2021-10-10 13:07:57 +0100

    js

--- JS/navbar.js
+++ JS/navbar.js
@@ -6,7 +6,7 @@ const interval = setInterval(function() {
     getQueue();
 }, 10000);
 
-function getQueue() {
+async function getQueue() {
     await fetch("https://2bqueue.info/*", {mode: "no-cors"}, {headers: {'Accept': 'application/json'}})
         .then(res => res.json())
         .then(data => {

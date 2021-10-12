4819d3f499254d37586041fdbda83b270673e433
Aquacks <ejrowett@gmail.com>
2021-10-12 21:10:42 +0100

    js

--- JS/home.js
+++ JS/home.js
@@ -1,6 +1,5 @@
-const securityDiv = document.getElementsByClassName("SAPlistElem")[0]
-
 document.addEventListener('DOMContentLoaded', function () {
+    const securityDiv = document.getElementsByClassName("SAPlistElem")[0]
     if(securityDiv){
         securityDiv.addEventListener("click", () => {
             securityDiv.setAttribute("style", "background-color: limegreen;");

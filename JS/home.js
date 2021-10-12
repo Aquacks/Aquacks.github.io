0eab711681bf2c9a09fde6a442ce32c39c2de0b9
Aquacks <ejrowett@gmail.com>
2021-10-12 21:26:59 +0100

    SAP

--- JS/home.js
+++ JS/home.js
@@ -5,4 +5,16 @@ document.addEventListener('DOMContentLoaded', function () {
             securityDiv.setAttribute("style", "background-color: limegreen;");
         })
     }
+    const automationDiv = document.getElementsByClassName("SAPlistElem")[1]
+    if(automationDiv){
+        automationDiv.addEventListener("click", () => {
+            automationDiv.setAttribute("style", "background-color: limegreen;");
+        })
+    }
+    const supplyDiv = document.getElementsByClassName("SAPlistElem")[2]
+    if(supplyDiv){
+        supplyDiv.addEventListener("click", () => {
+            supplyDiv.setAttribute("style", "background-color: limegreen;");
+        })
+    }
 });

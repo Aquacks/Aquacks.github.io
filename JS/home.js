07ad98904c1ef070a6e1406856857a1db5b84cd1
Aquacks <ejrowett@gmail.com>
2021-10-12 23:12:54 +0100

    imslow

--- JS/home.js
+++ JS/home.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     if(document.getElementsByClassName("SAPlistElem").length = 3){
         for(let i = 0; i <= document.getElementsByClassName("SAPlistElem").length; i++){
             if(document.getElementsByClassName("SAPlistElem")[i]){
-                document.getElementsByClassName("SAPlistElem")[i].addEventListener("hover", () => {              
+                document.getElementsByClassName("SAPlistElem")[i].addEventListener("mouseover", () => {              
                     document.getElementsByClassName("SAPlistElemContai")[i].setAttribute("style", "visibility: hidden;");
                 })
             }

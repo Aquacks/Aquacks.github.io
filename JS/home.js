7d77373ab83ffc66da4ecbde73fcf81449776cbe
Aquacks <ejrowett@gmail.com>
2021-10-12 20:52:40 +0100

    js

--- JS/home.js
+++ JS/home.js
@@ -1,11 +1,5 @@
-import $ from "jquery";
-
 const securityDiv = document.getElementsByClassName("SAPlistElem")[0]
 
-console.log(securityDiv)
-
-$(document).ready(function(){
-    $(securityDiv).click(function(){
-        alert("Clicked")
-    });
-});
+securityDiv.addEventListener("click", () => {
+    securityDiv.setAttribute("style", "background-color: limegreen;");
+})

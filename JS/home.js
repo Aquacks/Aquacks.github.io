03e9981b69fcc4b293d2b31b4b1741d7a1770393
Aquacks <ejrowett@gmail.com>
2021-10-12 22:15:05 +0100

    cssjs

--- JS/home.js
+++ JS/home.js
@@ -1,12 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const securityDiv = document.getElementsByClassName("SAPlistElem")[0]
-    const automationDiv = document.getElementsByClassName("SAPlistElem")[1]
-    const supplyDiv = document.getElementsByClassName("SAPlistElem")[2]
-
-    for(let i = 0; i <= document.getElementsByClassName("SAPlistElem").length; i++){
-        document.getElementsByClassName("SAPlistElem")[i].addEventListener("click", () => {
-            document.getElementsByClassName("SAPlistElem")[i].setAttribute("style", "background-color: limegreen;");
-            document.getElementsByClassName("SAPlistElem")[i].children.setAttribute("style", "visibility: hidden;");
-        })
+    if(document.getElementsByClassName("SAPlistElem").length > 2){
+        for(let i = 0; i <= document.getElementsByClassName("SAPlistElem").length; i++){
+            document.getElementsByClassName("SAPlistElem")[i].addEventListener("click", () => {
+                document.getElementsByClassName("SAPlistElem")[i].setAttribute("style", "background-color: limegreen;");
+                document.getElementsByClassName("SAPlistElem")[i].children.setAttribute("style", "visibility: hidden;");
+            })
+        }
     }
 });

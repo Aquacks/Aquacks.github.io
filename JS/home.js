40c1711e13320cc31c6f2f3c40fc02afafaa7aa0
Aquacks <ejrowett@gmail.com>
2021-10-12 21:58:42 +0100

    js

--- JS/home.js
+++ JS/home.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const automationDiv = document.getElementsByClassName("SAPlistElem")[1]
     const supplyDiv = document.getElementsByClassName("SAPlistElem")[2]
 
-    for(let i = 0; i <= document.getElementsByClassName("SAPlistElem"); i++){
+    for(let i = 0; i <= document.getElementsByClassName("SAPlistElem").length; i++){
         document.getElementsByClassName("SAPlistElem")[i].addEventListener("click", () => {
             document.getElementsByClassName("SAPlistElem")[i].setAttribute("style", "background-color: limegreen;");
             document.getElementsByClassName("SAPlistElem")[i].children.setAttribute("style", "visibility: hidden;");

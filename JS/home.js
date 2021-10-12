f11f3f927decaa3c5ab810d921493ee3fb182a7b
Aquacks <ejrowett@gmail.com>
2021-10-12 22:50:27 +0100

    js

--- JS/home.js
+++ JS/home.js
@@ -2,7 +2,9 @@ document.addEventListener('DOMContentLoaded', function () {
     if(document.getElementsByClassName("SAPlistElem").length = 3){
         for(let i = 0; i <= document.getElementsByClassName("SAPlistElem").length; i++){
             document.getElementsByClassName("SAPlistElem")[i].addEventListener("hover", () => {
-                document.getElementsByClassName("SAPlistElemContai")[i].setAttribute("style", "visibility: hidden;");
+                if(document.getElementsByClassName("SAPlistElem")[i]){
+                    document.getElementsByClassName("SAPlistElemContai")[i].setAttribute("style", "visibility: hidden;");
+                }
             })
         }
     }

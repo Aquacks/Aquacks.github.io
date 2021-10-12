34893a877c9b86af981c3897b4e86f7e1103f6bd
Aquacks <ejrowett@gmail.com>
2021-10-12 22:33:25 +0100

    divvis

--- JS/home.js
+++ JS/home.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-    if(document.getElementsByClassName("SAPlistElem").length > 2){
+    if(document.getElementsByClassName("SAPlistElem").length = 3){
         for(let i = 0; i <= document.getElementsByClassName("SAPlistElem").length; i++){
             document.getElementsByClassName("SAPlistElem")[i].addEventListener("click", () => {
                 document.getElementsByClassName("SAPlistElem")[i].setAttribute("style", "background-color: limegreen;");
-                document.getElementsByClassName("SAPlistElem")[i].children.setAttribute("style", "visibility: hidden;");
+                document.getElementsByClassName("SAPlistElemContai")[i].setAttribute("style", "visibility: hidden;");
             })
         }
     }

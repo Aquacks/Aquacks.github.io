5070858df34f1ea174873d840d7063f4c0814aee
Aquacks <ejrowett@gmail.com>
2021-10-13 18:23:21 +0100

    fade2electricboogaloo

--- JS/home.js
+++ JS/home.js
@@ -2,12 +2,14 @@ document.addEventListener('DOMContentLoaded', function () {
     if(document.getElementsByClassName("SAPlistElem").length = 3){
         for(let i = 0; i <= document.getElementsByClassName("SAPlistElem").length; i++){
             if(document.getElementsByClassName("SAPlistElem")[i]){
-                document.getElementsByClassName("SAPlistElem")[i].addEventListener("mouseenter", () => {       
-                    document.getElementsByClassName("SAPlistElemContai")[i].setAttribute("style", "visibility: hidden;");                                  
-                })
-                document.getElementsByClassName("SAPlistElem")[i].addEventListener("mouseleave", () => {                          
-                    document.getElementsByClassName("SAPlistElemContai")[i].setAttribute("style", "visibility: visible;");                                            
-                })
+                document.getElementsByClassName("SAPlistElem")[i].addEventListener("click", () => {   
+                    const elem = document.getElementsByClassName("SAPlistElemContai")[i];
+                    if($(elem).is(":hidden")){
+                        $(elem).fadeIn();
+                    }
+                    else{$(elem).fadeOut()}
+                    //document.getElementsByClassName("SAPlistElemContai")[i].setAttribute("style", "visibility: hidden;");                                  
+                })               
             }
         }
     }

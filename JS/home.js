e700afc64f4412604856c1350bc54c7a1d6316de
Aquacks <ejrowett@gmail.com>
2021-10-13 18:08:22 +0100

    js

--- JS/home.js
+++ JS/home.js
@@ -3,12 +3,10 @@ document.addEventListener('DOMContentLoaded', function () {
         for(let i = 0; i <= document.getElementsByClassName("SAPlistElem").length; i++){
             if(document.getElementsByClassName("SAPlistElem")[i]){
                 document.getElementsByClassName("SAPlistElem")[i].addEventListener("mouseenter", () => {       
-                    //document.getElementsByClassName("SAPlistElemContai")[i].setAttribute("style", "visibility: hidden;");                                  
-                    $(document.getElementsByClassName("SAPlistElemContai")[i]).fadeOut();
+                    document.getElementsByClassName("SAPlistElemContai")[i].setAttribute("style", "visibility: hidden;");                                  
                 })
-                document.getElementsByClassName("SAPlistElem")[i].addEventListener("mouseleave", () => {       
-                    $(document.getElementsByClassName("SAPlistElemContai")[i]).fadeIn();
-                    //document.getElementsByClassName("SAPlistElemContai")[i].setAttribute("style", "visibility: visible;");                                            
+                document.getElementsByClassName("SAPlistElem")[i].addEventListener("mouseleave", () => {                          
+                    document.getElementsByClassName("SAPlistElemContai")[i].setAttribute("style", "visibility: visible;");                                            
                 })
             }
         }

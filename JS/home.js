9751bddb10694d2bf4a0aa0d176e84a581039e41
Aquacks <ejrowett@gmail.com>
2021-10-13 20:27:16 +0100

    js

--- JS/home.js
+++ JS/home.js
@@ -2,10 +2,17 @@ document.addEventListener('DOMContentLoaded', function () {
     if(document.getElementsByClassName("SAPlistElem").length = 3){
         for(let i = 0; i <= document.getElementsByClassName("SAPlistElem").length; i++){
             if(document.getElementsByClassName("SAPlistElem")[i]){
-                document.getElementsByClassName("SAPlistElem")[i].addEventListener("click", () => {   
+                document.getElementsByClassName("SAPlistElem")[i].addEventListener("mouseenter", () => {   
                     const elem = document.getElementsByClassName("SAPlistElemContai")[i];
-                    $(elem).toggle(500);     
-                })               
+                       
+                }) 
+                document.getElementsByClassName("SAPlistElem")[i].addEventListener("mouseleave", () => {   
+                    const elem = document.getElementsByClassName("SAPlistElemContai")[i];
+                    if(elem.getAttribute("opacity") == 1){
+                        elem.setAttribute("style", "opacity:0;")
+                    }
+                    else {elem.setAttribute("style", "opacity:1;")}
+                })                   
             }
         }
     }

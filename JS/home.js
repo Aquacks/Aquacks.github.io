bed40ab4c3b42be63281fae829157d3b7a45e145
Aquacks <ejrowett@gmail.com>
2021-10-14 17:38:22 +0100

    js

--- JS/home.js
+++ JS/home.js
@@ -2,24 +2,14 @@ document.addEventListener('DOMContentLoaded', function () {
     if(document.getElementsByClassName("SAPlistElem").length = 3){
         for(let i = 0; i <= document.getElementsByClassName("SAPlistElem").length; i++){
             if(document.getElementsByClassName("SAPlistElem")[i]){
-                document.getElementsByClassName("SAPlistElem")[i].addEventListener("mouseenter", () => {   
+                document.getElementsByClassName("SAPlistElem")[i].addEventListener("click", () => {   
                     const elem = document.getElementsByClassName("SAPlistElemContainer")[i];
                     //fades old elements
-                    elem.setAttribute("style", "opacity:0;")   
-                    elem.setAttribute("style", "display:none;")
+                    $(elem).fadeOut()
                     //fades new elements                         
                     const textElem = document.getElementsByClassName("SAPtext")[i]
-                    textElem.setAttribute("style", "display:block")        
-                }) 
-                document.getElementsByClassName("SAPlistElem")[i].addEventListener("mouseleave", () => {   
-                    const elem = document.getElementsByClassName("SAPlistElemContainer")[i];
-                    //fades old elements
-                    elem.setAttribute("style", "opacity:1;")
-                    elem.setAttribute("style", "display:block;")                
-                    //unloads new text
-                    const textElem = document.getElementsByClassName("SAPtext")[i]
-                    textElem.setAttribute("style", "display:none")    
-                })                   
+                    $(textElem).fadeIn()       
+                })                               
             }
         }
     }

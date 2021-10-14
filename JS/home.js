20d2b7c258c6db6ce8ee1226a1dbfa7e4106fdd5
Aquacks <ejrowett@gmail.com>
2021-10-14 16:43:05 +0100

    sizing

--- JS/home.js
+++ JS/home.js
@@ -4,16 +4,18 @@ document.addEventListener('DOMContentLoaded', function () {
             if(document.getElementsByClassName("SAPlistElem")[i]){
                 document.getElementsByClassName("SAPlistElem")[i].addEventListener("mouseenter", () => {   
                     const elem = document.getElementsByClassName("SAPlistElemContainer")[i];
+                    //fades old elements
                     elem.setAttribute("style", "opacity:0;")   
-                    //fades show elements                         
+                    elem.setAttribute("style", "display:none;")
+                    //fades new elements                         
                     const textElem = document.getElementsByClassName("SAPtext")[i]
                     textElem.setAttribute("style", "display:block")        
                 }) 
                 document.getElementsByClassName("SAPlistElem")[i].addEventListener("mouseleave", () => {   
                     const elem = document.getElementsByClassName("SAPlistElemContainer")[i];
+                    //fades old elements
                     elem.setAttribute("style", "opacity:1;")
-                    //fades show elements
-
+                    elem.setAttribute("style", "display:block;")                
                     //unloads new text
                     const textElem = document.getElementsByClassName("SAPtext")[i]
                     textElem.setAttribute("style", "display:none")    

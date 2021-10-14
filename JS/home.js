51903984116e3aa00e9453684fa497c0e91d8b8b
Aquacks <ejrowett@gmail.com>
2021-10-14 17:47:41 +0100

    js

--- JS/home.js
+++ JS/home.js
@@ -2,13 +2,19 @@ document.addEventListener('DOMContentLoaded', function () {
     if(document.getElementsByClassName("SAPlistElem").length = 3){
         for(let i = 0; i <= document.getElementsByClassName("SAPlistElem").length; i++){
             if(document.getElementsByClassName("SAPlistElem")[i]){
+                var disToggl = true;
                 document.getElementsByClassName("SAPlistElem")[i].addEventListener("click", () => {   
-                    const elem = document.getElementsByClassName("SAPlistElemContainer")[i];
+                    const elem = document.getElementsByClassName("SAPlistElemContainer")[i];                 
                     //fades old elements
                     $(elem).fadeOut()
+                    if(disToggl == true){
+                        elem.setAttribute("stlye", "display:none;")
+                    }
+                    else {elem.setAttribute("stlye", "display:block;")}
                     //fades new elements                         
                     const textElem = document.getElementsByClassName("SAPtext")[i]
                     $(textElem).fadeIn()       
+                    disToggl = !disToggl;
                 })                               
             }
         }
